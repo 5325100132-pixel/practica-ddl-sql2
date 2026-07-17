4dedf823858f4d6e14b67b6b18260404f5528cf4
Jorge Luis <5325100132@correcaminos.uts.edu.mx>
2026-07-17 12:09:06 -0700

    Operaciones DML agregadas: Insert, Update, Delete

--- script_libreria.sql
+++ script_libreria.sql
@@ -1,19 +1,24 @@
+CREATE DATABASE Libreria
+GO
+USE Libreria
+GO
+
 -- 1. Creación de la tabla maestra
 CREATE TABLE Editoriales (
-	id_editorial INT IDENTITY(1,1) PRIMARY KEY,
-	nombre_editorial VARCHAR(50) NOT NULL,
-	pais_origen VARCHAR(50) NULL
+id_editorial INT IDENTITY(1,1) PRIMARY KEY,
+nombre_editorial VARCHAR(50) NOT NULL,
+pais_origen VARCHAR(50) NULL
 );
 GO
 
 -- 2. Creación de tabla con llave foránea
 CREATE TABLE Libros (
-	id_libro INT IDENTITY(1,1) PRIMARY KEY,
-	titulo VARCHAR(100) NOT NULL,
-	precio DECIMAL(10,2) NOT NULL,
-	id_editorial INT NOT NULL,
-	CONSTRAINT FK_Libro_Editorial FOREIGN KEY (id_editorial)
-	REFERENCES Editoriales(id_editorial)
+id_libro INT IDENTITY(1,1) PRIMARY KEY,
+titulo VARCHAR(100) NOT NULL,
+precio DECIMAL(10,2) NOT NULL,
+id_editorial INT NOT NULL,
+CONSTRAINT FK_Libro_Editorial FOREIGN KEY (id_editorial)
+REFERENCES Editoriales(id_editorial)
 );
 GO
 
@@ -24,8 +29,8 @@ GO
 
 -- 4. Destrucción de objetos
 CREATE TABLE Tabla_Basura (
-	id_basura INT PRIMARY KEY,
-	texto VARCHAR(20)
+id_basura INT PRIMARY KEY,
+texto VARCHAR(20)
 );
 GO
 
@@ -35,7 +40,7 @@ GO
 -- 5. Insertando registros
 INSERT INTO Editoriales (nombre_editorial, pais_origen)
 VALUES 	('Penguin Random House', 'Estados Unidos'),
-	('Planeta', 'España');
+('Planeta', 'España');
 GO
 
 INSERT INTO Libros (titulo, precio, id_editorial, anio_publicacion)
